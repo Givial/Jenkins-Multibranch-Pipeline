23f3ef1d2059e6c72e4608d5e0407fb255cfa010
Givial <carmen.g.viveros@accenture.com>
2022-12-08 15:14:17 -0600

    Script format updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,25 +8,31 @@ pipeline {
     stages{
          stage ('First') {
             steps {
+                script{
                 env.EXECUTE = 'true'
-                echo "Updating Second Stage to the Second Stage"  
+                }
+                echo 'Updating Second Stage to the Second Stage' 
                     }
                 }
                 stage ('Second') {
                     when {
+                        expression{
                          env.EXECUTE = 'true'
                         }
+                    }
                     steps {
-                        echo "The Second stage can be executed"
+                        echo 'The Second stage can be executed'
                     }
                 }
         
                 stage ('Third'){
                     when {
+                        expression {
                         env.EXECUTE != 'false'
+                        }
                     }
                     steps {
-                         echo "This stage has to be skipped"
+                         echo 'This stage has to be skipped'
                     }
                 }
     
